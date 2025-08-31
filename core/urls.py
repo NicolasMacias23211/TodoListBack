ca6c2509d88afd03099ee164e31c8298ea7645e2
Nicolas Duque <nicolas.duque23211@gmail.com>
2025-08-31 00:38:28 -0500

    se agrega el loggin

--- core/urls.py
+++ core/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import tasksViewSet , UserViewSet
+from .views import tasksViewSet , UserViewSet , LoginView
 
 router = DefaultRouter()
 router.register(r'tasks', tasksViewSet)
 router.register(r'users', UserViewSet)
 
 urlpatterns = [
+    path('login/', LoginView.as_view(), name='login'),
     path('', include(router.urls)),
 ]
--- core/views.py
+++ core/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from rest_framework import viewsets
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 from .models import tasks , users
 from .serializers import tasksSerializer , UserSerializer
 
@@ -10,3 +13,14 @@ class tasksViewSet(viewsets.ModelViewSet):
 class UserViewSet(viewsets.ModelViewSet):
     queryset = users.objects.all()
     serializer_class = UserSerializer
+
+class LoginView(APIView):
+    def post(self, request):
+        email = request.data.get('email')
+        password = request.data.get('password')
+        try:
+            user = users.objects.get(email=email, password=password)
+            serializer = UserSerializer(user)
+            return Response({'success': True, 'user': serializer.data})
+        except users.DoesNotExist:
+            return Response({'success': False, 'error': 'Credenciales inválidas'}, status=status.HTTP_401_UNAUTHORIZED)
